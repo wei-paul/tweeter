d80861d838c7d24578c27839c872385de9b4defc
wei-paul <paul.wei607@gmail.com>
2021-10-08 21:08:31 +0000

    Final commit of tweeter

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -2,26 +2,26 @@ $(document).ready(function() {
 
   //shows TWEET CONTAINER:
 
-  $(".buttontweet").on("click", function () {
+  $(".buttontweet").on("click", function() {
     $(".wholetextcontainer").show();
-  })
+  });
 
   //RENDER A TWEET AND APPENDING THE NEWLYCREATED TWEET(CHILD) TO PARENT.
   const renderTweets = function(tweets) {
     tweets.forEach((tweet) => {
-      const $newtweet = createTweetElement(tweet)
+      const $newtweet = createTweetElement(tweet);
       $('.tweets-container').prepend($newtweet);
-    })
+    });
 
-  }
+  };
 
-  const escape = function (str) {
+  const escape = function(str) {
     let div = document.createElement("div");
     div.appendChild(document.createTextNode(str));
     return div.innerHTML;
   };
  //CREATING A TWEET TEMPLATE
-  const createTweetElement = function (tweetData) {
+  const createTweetElement = function(tweetData) {
     const $tweet = $(`<article class="tweet-container">
             <header class="tweet-header">
               <div class="user-header">
@@ -47,28 +47,28 @@ $(document).ready(function() {
         </article>`);
     
     return $tweet;
-  }
+  };
   
 
 // FORM SUBMISSION USING JQUERY (AJAX REQUEST)
 
-  let url = 'http://localhost:8080/tweets'
+  let url = 'http://localhost:8080/tweets';
 
-  const loadtweets = function () {
+  const loadtweets = function() {
     $.ajax({
       url: url,
       method: "GET",
     })
       .then((result) => {
-        $('.tweets-container').html("")
-        const newTweet = result[result.length - 1]
+        $('.tweets-container').html("");
+        const newTweet = result[result.length - 1];
         renderTweets(result);
-      })
+      });
     
-  }
+  };
   loadtweets();
   
-  $("form").on("submit", function (event) {
+  $("form").on("submit", function(event) {
     const MAX_COUNTER = 140;
     event.preventDefault();
     $(".warning").hide();
@@ -87,8 +87,8 @@ $(document).ready(function() {
       })
 
       .catch((error) => {
-        console.log('error:', error)
-      })
-  })
+        console.log('error:', error);
+      });
+  });
 
 });
